5d69ccd532d5b02dafc67851d691db04787ad22b
julia-ln <julia.lechner@stud.hcw.ac.at>
2026-06-13 21:33:22 +0200

    Added vanishing woman anomaly

---
+++ Assets/Scripts/VanishingWoman.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class VanishingWoman : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject womanModel;
+    [SerializeField] private AudioSource optionalScareSound;
+
+    // This triggers automatically when an object enters our Box Collider
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object that stepped into the trigger is the Player
+        if (other.CompareTag("Player"))
+        {
+            // Make sure the model is actually active before vanishing her
+            if (womanModel != null && womanModel.activeSelf)
+            {
+                // Play a creepy sound effect right as she disappears (optional)
+                if (optionalScareSound != null)
+                {
+                    optionalScareSound.Play();
+                }
+
+                // Make her vanish!
+                womanModel.SetActive(false);
+                Debug.Log("Player got too close! The woman vanished.");
+            }
+        }
+    }
+}
